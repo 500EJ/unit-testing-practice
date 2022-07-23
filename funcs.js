24bbb1a83109739a1ac8cd470fd8f8105af9bd91
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2022-07-23 11:42:35 -0400

    Implement functions to pass specs

--- funcs.js
+++ funcs.js
@@ -1,13 +1,18 @@
 function isFive(num) {
-  // Your code here
+  return num === 5;
 }
 
 function isOdd(number) {
-  // Your code here
+  if (typeof number !== "number") throw new TypeError("Expected type number");
+  return number % 2 === 1 || number % 2 === -1;
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  const arr = [];
+  for (let i = min; i <= max; i += step) {
+    arr.push(i);
+  }
+  return arr;
 }
 
 module.exports = { isFive, isOdd, myRange };
