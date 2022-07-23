3b1b20770842324298ced5afa5263d61e819c355
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2022-07-23 11:51:34 -0400

    Add specs to Word class

--- test/class-spec.js
+++ test/class-spec.js
@@ -4,31 +4,39 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
+  let word;
+  beforeEach(() => (word = new Word("hello")));
+
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(word.word).to.exist;
     });
 
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(word.word).to.equal("hello");
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      expect(word.removeVowels()).to.equal("hll");
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      expect(word.removeConsonants()).to.equal("eo");
     });
   });
 
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      // Word begins with consonant
+      expect(word.pigLatin()).to.equal("ellohay");
+
+      // Word begins with vowel
+      word = new Word("apple");
+      expect(word.pigLatin()).to.equal("appleyay");
     });
   });
 });
